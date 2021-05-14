602a86abe5782d765450181932375d6cb98b7ef7
asjesus <asjesus@pmintl.net>
2021-05-14 16:08:51 +0100

    add src

---
+++ src/test/java/HelloWorld2Test.java
@@ -0,0 +1,10 @@
+public class HelloWorld2Test {
+
+    public void testHello(){
+        System.out.println("Test -Hello2.....");
+    }
+
+    public void testWorld(){
+        System.out.println("Test -World2.....");
+    }
+}
